d83db454c44a958599437128a43153ccd09c8687
OfficialAlok <sahalok001@gmail.com>
2024-09-01 20:30:36 +0530

    merge_sort updated

--- merge_sort.py
+++ merge_sort.py
@@ -6,6 +6,8 @@ def merge_sort(list):
     Divide: Find the midpoint of the list and divide into sublists 
     Conquer: Recursively sort the sublists created in previous step 
     Combine: merge the sorted sublists created in previous step
+
+    Takes O(n log n) time
     """
 
     if len(list) <= 1:
@@ -21,8 +23,57 @@ def split(list):
     """
     Divide the usorted list at midpoint into sublists
     Returns two sublists - left and right
+
+    Takes overall O(log n) time
     """
 
     mid = len(list) //2
     left = list[:mid]
     right = list[mid:]
+
+    return left, right
+
+def merge(left, right):
+    """
+    Merges two lists (arrays), sorting them in the process
+    Returns a new merged list
+
+    Runs overall in O(n) time
+    """
+
+    l = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1
+
+    while i < len(left):
+        l.append(left[i])
+        i+=1
+
+    while j < len(right):
+        l.append(right[j])
+        j+=1
+
+    return l
+
+
+def verify_sorted(list):
+    n = len(list)
+
+    if n == 0 or n == 1:
+        return True
+    
+    return list[0] < list[1] and verify_sorted(list[1:])
+
+
+alist = [54, 26 , 92, 17, 77 ,31, 44, 55, 20]
+l = merge_sort(alist)
+print(verify_sorted(alist))
+print(verify_sorted(l))
